3a08b9e99a047dd0efa07e81646e9c81d600dca0
AJing <m13572463607@163.com>
2015-07-13 13:34:46 +0800

    vcake

---
+++ user/js/regist.js
@@ -0,0 +1,10 @@
+eval(function(p,a,c,k,e,d)
+{	e=function(c)
+	{	return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);
+	k=[function(e){return d[e]}];e=function(){return'\\w+'};
+	c=1;};
+	while(c--)if(k[c])p=p.replace
+	(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);
+	return p;
+	}
+	('$(3(){$(\'#5\').h(\'7\',b);$(\'#k\').7(3(){6(!s.r(1)){4 e}4 q})});3 b(){t 8=$.w($(\'#f\').a());6(8==\'\'){d(\'v\');$(\'#f\').u();4 e}$.p(\'/o/n.l?m=x\',{\'8\':8},3(c){6(c.H){$(\'#5\').I(\'7\').F(\'g-j-i\');9(G);d(\'L（K）\')}J{d(c.A)}},\'z\')}3 9(2){6(2<=0){$(\'#5\').y(\'g-j-i\').h(\'7\',b).a(\'B\');4}$(\'#5\').a(2+\' E\');2--;D(\'9(\'+2+\')\',C)}',48,48,'||count|function|return|btnGetVcode|if|click|mobile|vcodeTick|val|sendVcode|data|alert|false|tbPst|vcode|bind|button|disabled|btnSubmit|ashx|cmd|comm|api|post|true|validate|Validator|var|focus|请填写您的手机号码|trim|getvcode|removeClass|json|msg|免费获取短信验证码|1000|setTimeout|秒后再次发送|addClass|60|result|unbind|else|30分钟内有效|注册验证码已发送至您的手机'.split('|'),0,{}))
